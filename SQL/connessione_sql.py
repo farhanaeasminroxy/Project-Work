1d8b2073b590cd9f725d6c16f1f161a66c2b4c00
farhanaeasminroxy <farhanaeasminroxy@gmail.com>
2026-02-16 15:54:24 +0100

    Add .gitignore and secure credentials

--- SQL/connessione_sql.py
+++ SQL/connessione_sql.py
@@ -30,3 +30,28 @@ cursor.execute("SELECT DATABASE();")
 print("Connected to database:", cursor.fetchone()[0])
 cursor.close()
 conn.close()
+# connessione file json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+import os
+import json
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+
+
+load_dotenv()
+
+# Firebase/Firestore setup
+credentials_json = os.getenv("GOOGLE_CREDENTIALS")
+credentials_dict = json.loads(credentials_json)
+
+cred = credentials.Certificate(credentials_dict)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+print("Firebase connected successfully!")
+
+# Now you can use both MySQL and Firestore
+# conn.close()  # Close when done
