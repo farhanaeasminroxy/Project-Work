e3abd449130a1ee4107cee7c0155c67cb414f0a2
farhanaeasminroxy <farhanaeasminroxy@gmail.com>
2026-02-16 10:13:08 +0100

    connesione con MySQL

--- SQL/connessione_sql.py
+++ SQL/connessione_sql.py
@@ -1,3 +1,4 @@
+'''
 import os
 from dotenv import load_dotenv
 import mysql.connector
@@ -7,4 +8,25 @@ conn = mysql.connector.connect(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
+)'''
+
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    auth_plugin='mysql_native_password'  # Add this line
 )
+
+print("Connection successful!")
+cursor = conn.cursor()
+cursor.execute("SELECT DATABASE();")
+print("Connected to database:", cursor.fetchone()[0])
+cursor.close()
+conn.close()
